af60c15b09d5c6345bc7f26a2ab410eff7a370d8
AnkyRep <thapliyal.ankit01@gmail.com>
2024-10-03 04:00:10 +0530

    End of ROuting

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,6 +12,7 @@ import { AccountService } from '../_services/account.service';
 })
 export class RegisterComponent {
   private accountService = inject(AccountService);
+  private toastr = inject(ToastrService);
   // @Input() userFromHomeComponent:any;
   @Output() cancelRegister = new EventEmitter();
 model :any = {};
@@ -22,7 +24,7 @@ register(){
       console.log(response);
       this.cancel();
     },
-    error : error => console.log(error)
+    error : error =>  this.toastr.error(error.error)
   })
 }
 
